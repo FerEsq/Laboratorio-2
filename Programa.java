3624f345ff4a47f0181cee840964b071d1b6e3a5
Fernanda Esquivel <esq21542@uvg.edu.gt>
2021-09-14 21:49:28 -0600

    Clase Programa y Memoria
    Se finalizó la clase Programa
    Se creó el archivo de la clase Memoria

---
+++ Programa.java
@@ -0,0 +1,25 @@
+
+/* Nombre: Progra,a.java
+ * Programador: Fernanda Esquivel (esq21542@uvg.edu.gt).
+ * Lenguaje: Java
+ * Recursos: Visual Studio Code
+ * Historial: Finalizado el xx.09.2021
+ 			  Modificado el xx.09.2021 */
+
+
+public class Programa 
+{
+    private String nombre;
+    private int espacio;
+    private int ciclos;
+    private int bloques;
+
+    public Programa(String n, int e, int c)
+    {
+        nombre = n;
+        espacio = e;
+        ciclos = c;
+
+    }
+    
+}
